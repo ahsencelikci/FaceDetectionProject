70fb676bc33fe849b182250cb5395f0318cae1cd
ahsencelikci <celikciahsen54@gmail.com>
2024-10-25 06:31:23 +0300

    comment

--- visualize.py
+++ visualize.py
@@ -5,18 +5,17 @@ from preprocess import load_data
 def visualize_samples():
     X_train, X_test, y_train, y_test = load_data()
     
-    # Görüntülerin boyutlarını ayarlamak için
-    height, width = 92, 112  # AT&T Face Database için standart boyutlar
 
-    # İlk 5 örneği göster
+    height, width = 92, 112  
+
+    
     for i in range(5):
         plt.subplot(1, 5, i + 1)
-        plt.imshow(X_test[i].reshape(height, width), cmap='gray')  # 1D diziyi 2D'ye çevir
+        plt.imshow(X_test[i].reshape(height, width), cmap='gray')  
         plt.title(f'Label: {y_test[i]}')
         plt.axis('off')
 
-    plt.show()  # Bu satırın olduğundan emin olun
-
+    plt.show()  
 
 if __name__ == "__main__":
     visualize_samples()
